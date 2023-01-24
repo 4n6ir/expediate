89b102e1735d14695cd7b09830d2250688336353
John Lukach <84751182+jblukach@users.noreply.github.com>
2023-01-24 02:55:27 +0000

    alert

--- expediate/expediate_stack.py
+++ expediate/expediate_stack.py
@@ -2,7 +2,15 @@ import cdk_nag
 
 from aws_cdk import (
     Aspects,
-    Stack
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_events as _events,
+    aws_events_targets as _targets,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs,
+    aws_logs_destinations as _destinations
 )
 
 from constructs import Construct
@@ -12,6 +20,9 @@ class ExpediateStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        account = Stack.of(self).account
+        region = Stack.of(self).region
+
         Aspects.of(self).add(
             cdk_nag.AwsSolutionsChecks(
                 log_ignores = True,
@@ -20,5 +31,102 @@ class ExpediateStack(Stack):
         )
 
         cdk_nag.NagSuppressions.add_stack_suppressions(
-            self, suppressions = []
+            self, suppressions = [
+                {'id': 'AwsSolutions-IAM4','reason': 'GitHub Issue'}
+            ]
+        )
+
+        layer = _lambda.LayerVersion.from_layer_version_arn(
+            self, 'layer',
+            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:getpublicip:3'
+        )
+
+### ERROR ###
+
+        error = _lambda.Function.from_function_arn(
+            self, 'error',
+            'arn:aws:lambda:'+region+':'+account+':function:shipit-error'
+        )
+
+        timeout = _lambda.Function.from_function_arn(
+            self, 'timeout',
+            'arn:aws:lambda:'+region+':'+account+':function:shipit-timeout'
+        )
+
+### IAM ###
+
+        role = _iam.Role(
+            self, 'role', 
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+
+### ALERT ###
+
+        alert = _lambda.Function(
+            self, 'alert',
+            handler = 'alert.handler',
+            code = _lambda.Code.from_asset('alert'),
+            architecture = _lambda.Architecture.ARM_64,
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            timeout = Duration.seconds(900),
+            environment = dict(
+                ACCOUNT = account,
+                REGION = region
+            ),
+            memory_size = 256,
+            role = role,
+            layers = [
+                layer
+            ]
+        )
+
+        alertlogs = _logs.LogGroup(
+            self, 'alertlogs',
+            log_group_name = '/aws/lambda/'+alert.function_name,
+            retention = _logs.RetentionDays.ONE_DAY,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+        alertsub = _logs.SubscriptionFilter(
+            self, 'alertsub',
+            log_group = alertlogs,
+            destination = _destinations.LambdaDestination(error),
+            filter_pattern = _logs.FilterPattern.all_terms('ERROR')
+        )
+
+        alerttime= _logs.SubscriptionFilter(
+            self, 'alerttime',
+            log_group = alertlogs,
+            destination = _destinations.LambdaDestination(error),
+            filter_pattern = _logs.FilterPattern.all_terms('Task','timed','out')
+        )
+
+        rule = _events.Rule(
+            self, 'rule',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.cloudtrail'],
+                detail = {
+                    "eventSource": [
+                        "cloudtrail.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DeleteVpc"
+                    ]
+                }
+            )
+        )
+
+        rule.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
         )
