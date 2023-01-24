049beb941b622fde8ae91487058d14d3534ae809
John Lukach <84751182+jblukach@users.noreply.github.com>
2023-01-24 02:05:33 +0000

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from expediate.expediate_stack import ExpediateStack
+
+app = cdk.App()
+
+ExpediateStack(
+    app, 'ExpediateStack',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = os.getenv('CDK_DEFAULT_REGION')
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+cdk.Tags.of(app).add('Alias','ALL')
+cdk.Tags.of(app).add('GitHub','https://github.com/jblukach/expediate.git')
+
+app.synth()
---
+++ expediate/expediate_stack.py
@@ -0,0 +1,24 @@
+import cdk_nag
+
+from aws_cdk import (
+    Aspects,
+    Stack
+)
+
+from constructs import Construct
+
+class ExpediateStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        Aspects.of(self).add(
+            cdk_nag.AwsSolutionsChecks(
+                log_ignores = True,
+                verbose = True
+            )
+        )
+
+        cdk_nag.NagSuppressions.add_stack_suppressions(
+            self, suppressions = []
+        )
