6ccffb07d1087dc0ac54515d25e1b7e2c8751588
John Lukach <84751182+jblukach@users.noreply.github.com>
2023-01-27 12:23:48 +0000

    events

--- alert/alert.py
+++ alert/alert.py
@@ -1,10 +1,53 @@
+import boto3
 import json
+import os
+from datetime import datetime, timezone
 
 def handler(event, context):
 
-    print(event)
+    print(event['detail']['eventName'])
+
+    account = os.environ['ACCOUNT']
+    region = os.environ['REGION']
+
+    now = datetime.now(timezone.utc).isoformat().replace('+00:00','Z')
+
+    securityhub_client = boto3.client('securityhub')
+
+    securityhub_response = securityhub_client.batch_import_findings(
+        Findings = [
+            {
+                "SchemaVersion": "2018-10-08",
+                "Id": region+"/"+account+"/alert",
+                "ProductArn": "arn:aws:securityhub:"+region+":"+account+":product/"+account+"/default", 
+                "GeneratorId": "ct-alert",
+                "AwsAccountId": account,
+                "CreatedAt": now,
+                "UpdatedAt": now,
+                "Title": "Alert",
+                "Description": str(event['detail']['eventName'])+" event in "+str(region)+" for account "+str(account),
+                "Resources": [
+                    {
+                        "Type": "AwsLambda",
+                        "Id": "arn:aws:lambda:"+region+":"+account+":function:alert"
+                    }
+                ],
+                "FindingProviderFields": {
+                    "Confidence": 100,
+                    "Severity": {
+                        "Label": "CRITICAL"
+                    },
+                    "Types": [
+                        "security/ct/alert"
+                    ]
+                }
+            }
+        ]
+    )
+
+    print(securityhub_response)
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Alert')
+        'body': json.dumps('Expediate Alert')
     }
--- expediate/expediate_stack.py
+++ expediate/expediate_stack.py
@@ -68,10 +68,22 @@ class ExpediateStack(Stack):
             )
         )
 
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    'securityhub:BatchImportFindings'
+                ],
+                resources = [
+                    'arn:aws:securityhub:'+region+':'+account+':product/'+account+'/default'
+                ]
+            )
+        )
+
 ### ALERT ###
 
         alert = _lambda.Function(
             self, 'alert',
+            function_name = 'alert',
             handler = 'alert.handler',
             code = _lambda.Code.from_asset('alert'),
             architecture = _lambda.Architecture.ARM_64,
@@ -111,6 +123,108 @@ class ExpediateStack(Stack):
 
 ### RULES ###
 
+        cloudshell = _events.Rule(
+            self, 'cloudshell',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.cloudshell'],
+                detail = {
+                    "eventSource": [
+                        "cloudshell.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateEnvironment",
+                        "CreateSession",
+                        "DeleteEnvironment",
+                        "GetEnvironmentStatus",
+                        "GetFileDownloadUrls",
+                        "GetFileUploadUrls",
+                        "PutCredentials",
+                        "StartEnvironment",
+                        "StopEnvironment"
+                    ]
+                }
+            )
+        )
+
+        cloudshell.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        cloudtrail = _events.Rule(
+            self, 'cloudtrail',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.cloudtrail'],
+                detail = {
+                    "eventSource": [
+                        "cloudtrail.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DeleteEventDataStore",
+                        "DeleteTrail",
+                        "PutEventSelectors",
+                        "StopLogging",
+                        "UpdateEventDataStore",
+                        "UpdateTrail"
+                    ]
+                }
+            )
+        )
+
+        cloudtrail.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        config = _events.Rule(
+            self, 'config',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.config'],
+                detail = {
+                    "eventSource": [
+                        "config.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DeleteDeliveryChannel",
+                        "StopConfigurationRecorder"
+                    ]
+                }
+            )
+        )
+
+        config.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        connect = _events.Rule(
+            self, 'connect',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.connect'],
+                detail = {
+                    "eventSource": [
+                        "connect.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateInstance"
+                    ]
+                }
+            )
+        )
+
+        connect.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
         ec2 = _events.Rule(
             self, 'ec2',
             event_pattern = _events.EventPattern(
@@ -121,13 +235,327 @@ class ExpediateStack(Stack):
                         "ec2.amazonaws.com"
                     ],
                     "eventName": [
-                        "DeleteVpc"
+                        "CreateDefaultVpc",
+                        "CreateImage",
+                        "CreateInstanceExportTask",
+                        "CreateKeyPair",
+                        "DeleteFlowLogs",
+                        "DeleteVpc",
+                        "DescribeInstanceAttribute",
+                        "DisableEbsEncryptionByDefault",
+                        "GetPasswordData",
+                        "ModifyInstanceAttribute",
+                        "ModifySnapshotAttribute",
+                        "SharedSnapshotCopyInitiated",
+                        "SharedSnapshotVolumeCreated"
                     ]
                 }
             )
         )
 
         ec2.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        ecr = _events.Rule(
+            self, 'ecr',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.ecr'],
+                detail = {
+                    "eventSource": [
+                        "ecr.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateRepository",
+                        "GetAuthorizationToken"
+                    ]
+                }
+            )
+        )
+
+        ecr.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        ecs = _events.Rule(
+            self, 'ecs',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.ecs'],
+                detail = {
+                    "eventSource": [
+                        "ecs.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "RegisterTaskDefinition",
+                        "RunTask"
+                    ]
+                }
+            )
+        )
+
+        ecs.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        eks = _events.Rule(
+            self, 'eks',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.eks'],
+                detail = {
+                    "eventSource": [
+                        "eks.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateCluster",
+                        "DeleteCluster"
+                    ]
+                }
+            )
+        )
+
+        eks.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        elasticache = _events.Rule(
+            self, 'elasticache',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.elasticache'],
+                detail = {
+                    "eventSource": [
+                        "elasticache.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "AuthorizeCacheSecurityGroupEgress",
+                        "AuthorizeCacheSecurityGroupIngress",
+                        "CreateCacheSecurityGroup",
+                        "DeleteCacheSecurityGroup",
+                        "RevokeCacheSecurityGroupEgress",
+                        "RevokeCacheSecurityGroupIngress"
+                    ]
+                }
+            )
+        )
+
+        elasticache.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        elasticfilesystem = _events.Rule(
+            self, 'elasticfilesystem',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.elasticfilesystem'],
+                detail = {
+                    "eventSource": [
+                        "elasticfilesystem.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DeleteFileSystem",
+                        "DeleteMountTarget"
+                    ]
+                }
+            )
+        )
+
+        elasticfilesystem.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        glue = _events.Rule(
+            self, 'glue',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.glue'],
+                detail = {
+                    "eventSource": [
+                        "glue.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateDevEndpoint",
+                        "DeleteDevEndpoint",
+                        "UpdateDevEndpoint"
+                    ]
+                }
+            )
+        )
+
+        glue.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        guardduty = _events.Rule(
+            self, 'guardduty',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.guardduty'],
+                detail = {
+                    "eventSource": [
+                        "guardduty.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "CreateIPSet"
+                    ]
+                }
+            )
+        )
+
+        guardduty.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        iam = _events.Rule(
+            self, 'iam',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.iam'],
+                detail = {
+                    "eventSource": [
+                        "iam.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "AddUserToGroup",
+                        "AttachGroupPolicy",
+                        "AttachUserPolicy",
+                        "ChangePassword",
+                        "CreateAccessKey",
+                        "CreateLoginProfile",
+                        "CreateUser",
+                        "CreateVirtualMFADevice",
+                        "DeactivateMFADevice",
+                        "DeleteAccessKey",
+                        "DeleteUser",
+                        "DeleteUserPolicy",
+                        "DeleteVirtualMFADevice",
+                        "DetachGroupPolicy",
+                        "DetachUserPolicy",
+                        "EnableMFADevice",
+                        "PutUserPolicy",
+                        "ResyncMFADevice",
+                        "UpdateAccessKey",
+                        "UpdateGroup",
+                        "UpdateLoginProfile",
+                        "UpdateSAMLProvider",
+                        "UpdateUser"
+                    ]
+                }
+            )
+        )
+
+        iam.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        kms = _events.Rule(
+            self, 'kms',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.kms'],
+                detail = {
+                    "eventSource": [
+                        "kms.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DisableKey",
+                        "ScheduleKeyDeletion"
+                    ]
+                }
+            )
+        )
+
+        kms.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        lambdaX = _events.Rule(
+            self, 'lambda',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.lambda'],
+                detail = {
+                    "eventSource": [
+                        "lambda.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "AddLayerVersionPermission",
+                        "CreateFunction",
+                        "GetLayerVersionPolicy",
+                        "PublishLayerVersion",
+                        "UpdateFunctionConfiguration"
+                    ]
+                }
+            )
+        )
+
+        lambdaX.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        macie = _events.Rule(
+            self, 'macie',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.macie'],
+                detail = {
+                    "eventSource": [
+                        "macie.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DisableMacie"
+                    ]
+                }
+            )
+        )
+
+        macie.add_target(
+            _targets.LambdaFunction(
+                alert
+            )
+        )
+
+        macie2 = _events.Rule(
+            self, 'macie2',
+            event_pattern = _events.EventPattern(
+                detail_type = ['AWS API Call via CloudTrail'],
+                source = ['aws.macie2'],
+                detail = {
+                    "eventSource": [
+                        "macie2.amazonaws.com"
+                    ],
+                    "eventName": [
+                        "DisableMacie"
+                    ]
+                }
+            )
+        )
+
+        macie2.add_target(
             _targets.LambdaFunction(
                 alert
             )
